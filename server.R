0ea9e47aaf5653713a342c4a08bf0fd2ecf757d2
thomascjohnson <thomascjohnson@gmail.com>
2017-10-29 14:42:51 -0400

    Adding progress bar

--- server.R
+++ server.R
@@ -8,37 +8,39 @@ library(dygraphs)
 library(xts)
 
 shinyServer(function(input, output) {
-  logs <- list.files(path = "Data", pattern = "^[0-9]{8}[.](CSV|csv)",
-                     full.names = TRUE)
-  res_octo_data <- Reduce(function(csv1, csv2) {
-    merge(csv1, csv2, all = TRUE)
-  }, lapply(logs, read_csv, col_names = columns))
+  withProgress("Loading data", {
+    logs <- list.files(path = "Data", pattern = "^[0-9]{8}[.](CSV|csv)",
+                       full.names = TRUE)
+    res_octo_data <- Reduce(function(csv1, csv2) {
+      merge(csv1, csv2, all = TRUE)
+    }, lapply(logs, read_csv, col_names = columns))
 
-  res_octo_data %<>% mutate(Date = paste(Date, as.character(Time)) %>%
-                              as.POSIXct(format = "%d/%m/%Y %H:%M:%S",
-                                         tz = "GMT")) %>%
-    select(-Time)
+    res_octo_data %<>% mutate(Date = paste(Date, as.character(Time)) %>%
+                                as.POSIXct(format = "%d/%m/%Y %H:%M:%S",
+                                           tz = "GMT")) %>%
+      select(-Time)
 
-  res_octo_data %<>%
-    gather(SensorType, Value, -Temp, -var1, -var2, -Voltage, -Date) %>%
-    mutate(Specimen = factor(gsub("_\\w+_\\d+$", "", SensorType)),
-           SensorNumber = factor(gsub("^\\d+_\\w+_", "", SensorType)),
-           SensorType = factor(gsub("(\\d_|_\\d)", "", SensorType)))
+    res_octo_data %<>%
+      gather(SensorType, Value, -Temp, -var1, -var2, -Voltage, -Date) %>%
+      mutate(Specimen = factor(gsub("_\\w+_\\d+$", "", SensorType)),
+             SensorNumber = factor(gsub("^\\d+_\\w+_", "", SensorType)),
+             SensorType = factor(gsub("(\\d_|_\\d)", "", SensorType)))
+  })
 
-    values <- reactive({
-      res_octo_data %>% filter(Specimen == input$specimen &
-                                       ((SensorNumber == input$depth &
-                                           SensorType == "depth") |
-                                          (SensorNumber == input$thermistor &
-                                             SensorType == "thermistor"))) %>%
-      distinct() %>%
-      select(-SensorNumber) %>%
-      spread(SensorType, Value) %>%
-      mutate(Corrected = correctValue(depth, as.numeric(input$ea), rg,
-                                       as.numeric(input$tref),
-                                       temp(thermistor))) %>%
-      drop_na()
-    })
+  values <- reactive({
+    res_octo_data %>% filter(Specimen == input$specimen &
+                                     ((SensorNumber == input$depth &
+                                         SensorType == "depth") |
+                                        (SensorNumber == input$thermistor &
+                                           SensorType == "thermistor"))) %>%
+    distinct() %>%
+    select(-SensorNumber) %>%
+    spread(SensorType, Value) %>%
+    mutate(Corrected = correctValue(depth, as.numeric(input$ea), rg,
+                                     as.numeric(input$tref),
+                                     temp(thermistor))) %>%
+    drop_na()
+  })
   output$resistancePlot <- renderDygraph({
     vals <- values()
     cvalsts <- xts(vals$Corrected, vals$Date)
